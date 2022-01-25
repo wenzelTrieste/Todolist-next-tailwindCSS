d838b326bba2e96bc39ef484efdaf847e685a1ef
Wenzel Trieste <wenzelconta2018@gmail.com>
2022-01-25 10:03:22 -0300

    Transferindo tudo que já foi feito no Todolist para o github

---
+++ src/components/Botao.tsx
@@ -0,0 +1,22 @@
+interface BotaoProps {
+    cor?: "green" | "blue" | "gray";
+    className: string;
+    children: any;
+  }
+  
+  export default function Botao(props: BotaoProps) {
+    const cor = props.cor ?? 'gray';
+  
+    return (
+      <button
+        className={`
+              bg-gradient-to-r from-green-400 to-green-700
+              text-white px-4 py-2 rounded-md
+              ${props.className}
+          `}
+      >
+        {props.children}
+      </button>
+    );
+  }
+  
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: {
+
+    content: [
+      "./src/pages/**/*.{js,ts,jsx,tsx}",
+      "./src/components/**/*.{js,ts,jsx,tsx}",
+    ],
+    safeList: [
+      /^bg-/,
+      /^to-/,
+      /^from-/,
+    ]
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
