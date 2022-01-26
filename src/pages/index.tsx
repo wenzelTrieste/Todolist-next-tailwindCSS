fe803aa960ef0f1aa945cd723d1ee61d6dfb32a0
Wenzel Trieste <wenzelconta2018@gmail.com>
2022-01-26 09:15:27 -0300

    Integração com o Firebase CRUD

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,9 @@ import Tabela from "../components/Tabela";
 import Cliente from "../core/Cliente";
 
 export default function Home() {
+  const [visivel, setVisivel] = useState<"tabela" | "form">("tabela");
+  const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
+  
   const clientes = [
     new Cliente("Ana", 34, "1"),
     new Cliente("Bia", 45, "2"),
@@ -14,7 +17,12 @@ export default function Home() {
   ];
 
   function clienteSelecionado(cliente: Cliente) {
-    console.log(cliente.nome);
+    setCliente(cliente)
+    setVisivel('form')
+  }
+  function novoCliente() {
+    setCliente(Cliente.vazio())
+    setVisivel('form')
   }
   function clienteExcluido(cliente: Cliente) {
     console.log(`Exclui: ${cliente.nome}`);
@@ -22,9 +30,8 @@ export default function Home() {
 
   function salvarCliente(cliente: Cliente) {
     console.log(cliente)
+    setVisivel('tabela')
   }
-
-  const [visivel, setVisivel] = useState<"tabela" | "form">("tabela");
 
   return (
     <div
@@ -38,7 +45,7 @@ export default function Home() {
         {visivel === "tabela" ? (
           <>
             <div className="flex justify-end">
-              <Botao cor="green" className="mb-4" onClick={() => setVisivel('form')}>
+              <Botao cor="green" className="mb-4" onClick={novoCliente}>
                 Novo Cliente
               </Botao>
             </div>
@@ -49,7 +56,7 @@ export default function Home() {
             ></Tabela>
           </>
         ) : (
-          <Formulario cliente={clientes[2]}
+          <Formulario cliente={cliente}
           clienteMudou={salvarCliente}
           cancelado={() => setVisivel('tabela')}/>
         )}
